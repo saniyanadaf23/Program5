09ee0e2cbfa9e9b31cc7667c1c117d026a812d5f
qanusha436 <amoghajoshi2005@gmail.com>
2025-05-13 15:58:31 +0530

    save

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; 
+        int a = 0, b = 1;
+        
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " "); 
+            int next = a + b; 
+            a = b;
+            b = next;
+        }
+    }
+}
